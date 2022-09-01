6fadb9a8a68b24c2886b36b4592a71a5dbf3c6e4
Rachmannh <rachmannul1999@gmail.com>
2022-09-01 20:51:41 +0700

    commit

---
+++ assets/js/dropdown.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".navbar-default .navbar-nav > li.dropdown").hover(
+    function () {
+      $("ul.dropdown-menu", this).stop(true, true).slideDown("fast");
+      $(this).addClass("open");
+    },
+    function () {
+      $("ul.dropdown-menu", this).stop(true, true).slideUp("fast");
+      $(this).removeClass("open");
+    }
+  );
+});
