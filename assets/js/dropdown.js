0685c2ae0dce62725fbb5b86b3ebc1a4c983f642
Rachmannh <rachmannul1999@gmail.com>
2022-09-02 10:41:04 +0700

    commit minifier file

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(".navbar-default .navbar-nav > li.dropdown").hover(
     function () {
-      $("ul.dropdown-menu", this).stop(true, true).slideDown("fast");
+      $("ul.dropdown-menu", this).stop(!0, !0).slideDown("fast");
       $(this).addClass("open");
     },
     function () {
-      $("ul.dropdown-menu", this).stop(true, true).slideUp("fast");
+      $("ul.dropdown-menu", this).stop(!0, !0).slideUp("fast");
       $(this).removeClass("open");
     }
   );
--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -4,13 +4,7 @@ gsap.from(".navbar", {
   y: "-100%",
   ease: "elastic.inOut(1, 5)",
 });
-
-gsap.from(".my-hero", {
-  duration: 3,
-  opacity: 0,
-  ease: "elastic.inOut(1, 5)",
-});
-
+gsap.from(".my-hero", { duration: 3, opacity: 0, ease: "elastic.inOut(1, 5)" });
 gsap.from(".hero-title", {
   duration: 2,
   opacity: 0,
@@ -18,7 +12,6 @@ gsap.from(".hero-title", {
   ease: "elastic.inOut(1, 5)",
   delay: 1,
 });
-
 gsap.from(".hero-paragraph", {
   duration: 2,
   opacity: 0,
@@ -27,7 +20,6 @@ gsap.from(".hero-paragraph", {
   ease: "elastic.inOut(1, 5)",
   delay: 2,
 });
-
 gsap.from(".hero-buttons", {
   duration: 2,
   opacity: 0,
