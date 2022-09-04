ea2b025d9aa0357ed74caed3982ef96c0c6b1ac2
Rachmannh <rachmannul1999@gmail.com>
2022-09-04 07:24:02 +0700

    commit

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -4,26 +4,35 @@ gsap.from(".navbar", {
   y: "-100%",
   ease: "bounce.inOut(1, 5)",
 });
-gsap.from(".my-hero", { duration: 3, opacity: 0, ease: "elastic.inOut(1, 5)" });
-gsap.from(".hero-title", {
+
+gsap.from(".hero-heading", {
+  duration: 2,
+  opacity: 0,
+  y: "100%",
+  ease: "elastic.inOut(1, 5)",
+  delay: 1,
+});
+
+gsap.from(".hero-sub-heading", {
   duration: 2,
   opacity: 0,
   x: "-100%",
   ease: "elastic.inOut(1, 5)",
-  delay: 1,
-});
-gsap.from(".hero-paragraph", {
-  duration: 2,
-  opacity: 0,
-  x: "100%",
-  y: "-150%",
-  ease: "elastic.inOut(1, 5)",
   delay: 2,
 });
+
 gsap.from(".hero-buttons", {
   duration: 2,
   opacity: 0,
   y: "100%",
   ease: "elastic.inOut(1, 5)",
   delay: 2,
+});
+
+gsap.from(".hero-images", {
+  duration: 2,
+  opacity: 0,
+  x: "-100%",
+  ease: "elastic.InOut(4, 2)",
+  delay: 3,
 });
